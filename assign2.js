683fc519d0f4aa0cf95796085aa8daf4f7f6b6ad
micalev <alevi561@mtroyal.ca>
2023-11-28 17:34:40 -0700

    Added functionality for playlist (incomplete)

--- assign2.js
+++ assign2.js
@@ -149,34 +149,244 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   // This function populates the genre and artist dropdown in the search form
-    function populateDropdowns() {
-      const songsData = getSongsData();
-      if (songsData) {
-        const artistDropdown = document.querySelector('#artistDropdown');
-        const genreDropdown = document.querySelector('#genreDropdown');
+  function populateDropdowns() {
+    const songsData = getSongsData();
+    if (songsData) {
+      const artistDropdown = document.querySelector('#artistDropdown');
+      const genreDropdown = document.querySelector('#genreDropdown');
 
-        const uniqueArtists = [...new Set(songsData.map(song => song.artist.name))];
-        const uniqueGenres = [...new Set(songsData.map(song => song.genre.name))];
+      const uniqueArtists = [...new Set(songsData.map(song => song.artist.name))];
+      const uniqueGenres = [...new Set(songsData.map(song => song.genre.name))];
 
-        // Clear previous options
-        artistDropdown.innerHTML = '';
-        genreDropdown.innerHTML = '';
+      // Clear previous options
+      artistDropdown.innerHTML = '';
+      genreDropdown.innerHTML = '';
 
-        // Add default option
-        artistDropdown.innerHTML += `<option value="" disabled selected>Pick one</option>`;
-        genreDropdown.innerHTML += `<option value="" disabled selected>Pick one</option>`;
+      // Add default option
+      artistDropdown.innerHTML += `<option value="" disabled selected>Pick one</option>`;
+      genreDropdown.innerHTML += `<option value="" disabled selected>Pick one</option>`;
 
-        // Add options based on unique artist names
-        uniqueArtists.forEach(artist => {
-          artistDropdown.innerHTML += `<option value="${artist}">${artist}</option>`;
-        });
+      // Add options based on unique artist names
+      uniqueArtists.forEach(artist => {
+        artistDropdown.innerHTML += `<option value="${artist}">${artist}</option>`;
+      });
 
-        // Add options based on unique genre names
-        uniqueGenres.forEach(genre => {
-          genreDropdown.innerHTML += `<option value="${genre}">${genre}</option>`;
-        });
+      // Add options based on unique genre names
+      uniqueGenres.forEach(genre => {
+        genreDropdown.innerHTML += `<option value="${genre}">${genre}</option>`;
+      });
+    }
+  }
+
+  // Function to render the song details for display on search page
+function renderSongDetails(songs) {
+  let songTitle;
+  let songDetailsHTML = '';
+
+  songs.forEach(song => {
+    let displayedTitle = song.title.length > 25 ? `${song.title.slice(0, 25)}` : song.title;
+
+    const shortTitle = `
+      <a href="#" class="songTitleHyperlink">${displayedTitle}</a>
+      <span class="spantool" data-title="${song.title}"></span>
+    `;
+
+    const longTitle = `
+      <a href="#" class="songTitleHyperlink">${displayedTitle}</a>
+      <span class="spantool" data-title="${song.title}">&hellip;</span>
+    `;
+
+    if (song.title.length > 25) {
+      songTitle = longTitle;
+    } else {
+      songTitle = shortTitle;
+    }
+
+    songDetailsHTML += `
+      <tr>
+        <td class="c1">${songTitle}</td>
+        <td class="c2">${song.artist.name}</td>
+        <td class="c3">${song.year}</td>
+        <td class="c4">${song.genre.name}</td>
+        <td class="c5"><button type="button" id="addButton" data-songid="${song.song_id}">Add</button></td>
+      </tr>
+    `;
+    
+  });
+
+  return songDetailsHTML;
+  
+}
+
+// Function to handle song title click event
+function handleSongTitleClick(songs) {
+  const songDetailsDiv = document.querySelector('#songDetails');
+
+  // Event listener for the song title hyperlink
+  songDetailsDiv.addEventListener('click', event => {
+    event.preventDefault();
+
+    const target = event.target;
+    if (target.classList.contains('songTitleHyperlink')) {
+      const fullTitle = target.nextElementSibling.getAttribute('data-title');
+      const selectedSong = songs.find(song => song.title === fullTitle);
+
+      if (selectedSong) {
+        displaySingleSongView(selectedSong);
       }
     }
+  });
+}
+
+// Function to handle ellipsis click event
+function handleEllipsisClick() {
+  const songDetailsDiv = document.querySelector('#songDetails');
+
+  // Event listener for the spantool when user clicks on the ellipsis
+  songDetailsDiv.addEventListener('click', event => {
+    const target = event.target;
+    if (target.classList.contains('spantool')) {
+      event.stopPropagation();
+
+      const fullTitle = target.getAttribute('data-title');
+      const spantool = document.createElement('div');
+      spantool.classList.add('spantool-popup');
+      spantool.textContent = fullTitle;
+
+      const rect = target.getBoundingClientRect();
+      spantool.style.position = 'absolute';
+      spantool.style.top = `${rect.bottom + window.scrollY}px`;
+      spantool.style.left = `${rect.right + window.scrollX}px`;
+
+      document.body.appendChild(spantool);
+
+      setTimeout(() => {
+        spantool.remove();
+      }, 5000);
+    }
+  });
+  }
+
+  // Function to set up column header click listeners for sorting
+function setUpSortingListeners(songs) {
+  document.querySelectorAll('.sort-button').forEach(button => {
+    button.addEventListener('click', () => {
+      const sortBy = button.classList[1].replace('sort-by-', ''); // Extract the column to sort by
+      sortSongsBy(sortBy, songs);
+    });
+  });
+}
+
+// Function to display the search results
+function displaySearchResults(songs) {
+  const songDetailsDiv = document.querySelector('#songDetails');
+  songDetailsDiv.innerHTML = '';
+
+  const songDetailsHTML = renderSongDetails(songs);
+  songDetailsDiv.innerHTML = songDetailsHTML;
+
+  handleSongTitleClick(songs);
+  handleEllipsisClick();
+  setUpSortingListeners(songs);
+}
+ 
+// Playlist stuff
+
+// Function to render the playlist table
+let playlist = [];
+
+function renderPlaylist(playlistSongs) {
+  console.log(`Number of songs in the playlist: ${playlistSongs.length}`); // Log the number of songs in the playlist
+
+  const playlistBody = document.querySelector('#playlistBody');
+  const playlistCount = document.querySelector('#playlistCount'); // New element for displaying count
+  playlistBody.innerHTML = ''; // Clear previous playlist content
+
+  playlistSongs.forEach(song => {
+    playlistBody.innerHTML += `
+      <tr>
+        <td class="c1">${song.title}</td>
+        <td class="c2">${song.artist.name}</td>
+        <td class="c3">${song.year}</td>
+        <td class="c4">${song.genre.name}</td>
+        <td class="c5"></td>
+      </tr>
+    `;
+    console.log(`rendering song: ${song.title}`);
+  });
+
+  // Update the count of songs in the playlist
+  playlistCount.textContent = `Total Songs in Playlist: ${playlistSongs.length}`;
+  return playlistBody.innerHTML;
+}
+
+// Function to check if a song is already in the playlist
+function isSongInPlaylist(song) {
+  return playlist.some(item => item.id === song.id);
+}
+// Function to get the playlist from local storage or initialize it
+function getOrCreatePlaylist() {
+  let storedPlaylist = localStorage.getItem('playlist');
+  if (!storedPlaylist || storedPlaylist === '[]') {
+    // Initialize playlist if not found or empty
+    playlist = [];
+  } else {
+    playlist = JSON.parse(storedPlaylist);
+  }
+}
+
+// Call the function to get or initialize the playlist
+getOrCreatePlaylist();
+
+// Event listener for adding songs to the playlist (using event delegation)
+document.addEventListener('click', function(event) {
+  if (event.target.id === 'addButton') {
+    const songId = event.target.getAttribute('data-songid');
+    const selectedSong = songsData.find(song => song.song_id === parseInt(songId));
+    console.log(`${selectedSong.title}`);
+    
+    if (selectedSong && !isSongInPlaylist(selectedSong)) {
+      playlist.push(selectedSong); // Add the song to the playlist array
+      displayPlaylist(playlist); // Render the updated playlist
+      // Update local storage with the updated playlist
+      
+      // Update local storage with the updated playlist
+      localStorage.setItem('playlist', JSON.stringify(playlist));
+      console.log("Added song to playlist");
+    } else {
+      console.log(`Song "${selectedSong.title}" is already in the playlist.`);
+    }
+  }
+});
+
+// Function to display the playlist
+function displayPlaylist(playlist) {
+  const playlistBody = document.querySelector('#playlistBody');
+  playlistBody.innerHTML = '';
+
+  const playlistHTML = renderPlaylist(playlist);
+  playlistBody.innerHTML = playlistHTML;
+
+  // call the remove button function here
+}
+
+// Add an event listener for the Clear Playlist button
+document.getElementById('clearPlaylistButton').addEventListener('click', function() {
+  clearPlaylist();
+});
+
+// Function to clear the playlist
+function clearPlaylist() {
+  playlist = []; // Clear the playlist array by assigning it an empty array
+  renderPlaylist(playlist); // Update the rendered playlist to reflect the changes
+  localStorage.removeItem('playlist'); // Remove the playlist from local storage
+}
+
+
+
+
+
+
 });
 
 
--- searchResult.js
+++ searchResult.js
@@ -1,109 +1,129 @@
-// Function to render the song details for display on search page
-function renderSongDetails(songs) {
-  let songTitle;
-  let songDetailsHTML = '';
+// // Function to render the song details for display on search page
+// function renderSongDetails(songs) {
+//   let songTitle;
+//   let songDetailsHTML = '';
 
-  songs.forEach(song => {
-    let displayedTitle = song.title.length > 25 ? `${song.title.slice(0, 25)}` : song.title;
+//   songs.forEach(song => {
+//     let displayedTitle = song.title.length > 25 ? `${song.title.slice(0, 25)}` : song.title;
 
-    const shortTitle = `
-      <a href="#" class="songTitleHyperlink">${displayedTitle}</a>
-      <span class="spantool" data-title="${song.title}"></span>
-    `;
+//     const shortTitle = `
+//       <a href="#" class="songTitleHyperlink">${displayedTitle}</a>
+//       <span class="spantool" data-title="${song.title}"></span>
+//     `;
 
-    const longTitle = `
-      <a href="#" class="songTitleHyperlink">${displayedTitle}</a>
-      <span class="spantool" data-title="${song.title}">&hellip;</span>
-    `;
+//     const longTitle = `
+//       <a href="#" class="songTitleHyperlink">${displayedTitle}</a>
+//       <span class="spantool" data-title="${song.title}">&hellip;</span>
+//     `;
 
-    if (song.title.length > 25) {
-      songTitle = longTitle;
-    } else {
-      songTitle = shortTitle;
-    }
+//     if (song.title.length > 25) {
+//       songTitle = longTitle;
+//     } else {
+//       songTitle = shortTitle;
+//     }
 
-    songDetailsHTML += `
-      <tr>
-        <td class="c1">${songTitle}</td>
-        <td class="c2">${song.artist.name}</td>
-        <td class="c3">${song.year}</td>
-        <td class="c4">${song.genre.name}</td>
-        <td class="c5"><button type="button" class="addButton">Add</button></td>
-      </tr>
-    `;
-  });
+//     songDetailsHTML += `
+//       <tr>
+//         <td class="c1">${songTitle}</td>
+//         <td class="c2">${song.artist.name}</td>
+//         <td class="c3">${song.year}</td>
+//         <td class="c4">${song.genre.name}</td>
+//         <td class="c5"><button type="button" id="addButton" data-songid="${song.song_id}">Add</button></td>
+//       </tr>
+//     `;
+    
+//   });
 
-  return songDetailsHTML;
-}
+//   return songDetailsHTML;
+  
+// }
 
-// Function to handle song title click event
-function handleSongTitleClick(songs) {
-  const songDetailsDiv = document.querySelector('#songDetails');
+// // Function to handle song title click event
+// function handleSongTitleClick(songs) {
+//   const songDetailsDiv = document.querySelector('#songDetails');
 
-  // Event listener for the song title hyperlink
-  songDetailsDiv.addEventListener('click', event => {
-    event.preventDefault();
+//   // Event listener for the song title hyperlink
+//   songDetailsDiv.addEventListener('click', event => {
+//     event.preventDefault();
 
-    const target = event.target;
-    if (target.classList.contains('songTitleHyperlink')) {
-      const fullTitle = target.nextElementSibling.getAttribute('data-title');
-      const selectedSong = songs.find(song => song.title === fullTitle);
+//     const target = event.target;
+//     if (target.classList.contains('songTitleHyperlink')) {
+//       const fullTitle = target.nextElementSibling.getAttribute('data-title');
+//       const selectedSong = songs.find(song => song.title === fullTitle);
 
-      if (selectedSong) {
-        displaySingleSongView(selectedSong);
-      }
-    }
-  });
-}
+//       if (selectedSong) {
+//         displaySingleSongView(selectedSong);
+//       }
+//     }
+//   });
+// }
 
-// Function to handle ellipsis click event
-function handleEllipsisClick() {
-  const songDetailsDiv = document.querySelector('#songDetails');
+// // Function to handle ellipsis click event
+// function handleEllipsisClick() {
+//   const songDetailsDiv = document.querySelector('#songDetails');
 
-  // Event listener for the spantool when user clicks on the ellipsis
-  songDetailsDiv.addEventListener('click', event => {
-    const target = event.target;
-    if (target.classList.contains('spantool')) {
-      event.stopPropagation();
+//   // Event listener for the spantool when user clicks on the ellipsis
+//   songDetailsDiv.addEventListener('click', event => {
+//     const target = event.target;
+//     if (target.classList.contains('spantool')) {
+//       event.stopPropagation();
 
-      const fullTitle = target.getAttribute('data-title');
-      const spantool = document.createElement('div');
-      spantool.classList.add('spantool-popup');
-      spantool.textContent = fullTitle;
+//       const fullTitle = target.getAttribute('data-title');
+//       const spantool = document.createElement('div');
+//       spantool.classList.add('spantool-popup');
+//       spantool.textContent = fullTitle;
 
-      const rect = target.getBoundingClientRect();
-      spantool.style.position = 'absolute';
-      spantool.style.top = `${rect.bottom + window.scrollY}px`;
-      spantool.style.left = `${rect.right + window.scrollX}px`;
+//       const rect = target.getBoundingClientRect();
+//       spantool.style.position = 'absolute';
+//       spantool.style.top = `${rect.bottom + window.scrollY}px`;
+//       spantool.style.left = `${rect.right + window.scrollX}px`;
 
-      document.body.appendChild(spantool);
+//       document.body.appendChild(spantool);
 
-      setTimeout(() => {
-        spantool.remove();
-      }, 5000);
-    }
-  });
-}
+//       setTimeout(() => {
+//         spantool.remove();
+//       }, 5000);
+//     }
+//   });
+// }
 
-// Function to set up column header click listeners for sorting
-function setUpSortingListeners(songs) {
-  document.querySelectorAll('.sort-button').forEach(button => {
-    button.addEventListener('click', () => {
-      const sortBy = button.classList[1].replace('sort-by-', ''); // Extract the column to sort by
-      sortSongsBy(sortBy, songs);
-    });
-  });
-}
+// // function addButtonClick() {
+// //   const addButton = document.querySelector('#addButton');
+// //   addButton.addEventListener('click', event => {
+// //     const target = event.target;
 
-// Function to display the search results
-function displaySearchResults(songs) {
-  const songDetailsDiv = document.querySelector('#songDetails');
-  songDetailsDiv.innerHTML = '';
+// //         if (event.target.classList.contains('addButton')) {
+// //           const songId = event.target.getAttribute('data-songid');
+// //           const songToAdd = songs.find(song => song.song_id === parseInt(songId, 10));
+// //           console.log("button has been clicked");
+// //           if (songToAdd) {
+// //             addToPlaylist(songToAdd);
+// //             // You can update UI or perform any further operations after adding to the playlist
+// //             console.log(songToAdd);
+// //           }
+// //         }
+// //     });
+// //   }
+    
+// // // Function to set up column header click listeners for sorting
+// // function setUpSortingListeners(songs) {
+// //   document.querySelectorAll('.sort-button').forEach(button => {
+// //     button.addEventListener('click', () => {
+// //       const sortBy = button.classList[1].replace('sort-by-', ''); // Extract the column to sort by
+// //       sortSongsBy(sortBy, songs);
+// //     });
+// //   });
+// // }
 
-  const songDetailsHTML = renderSongDetails(songs);
-  songDetailsDiv.innerHTML = songDetailsHTML;
+// // // Function to display the search results
+// // function displaySearchResults(songs) {
+// //   const songDetailsDiv = document.querySelector('#songDetails');
+// //   songDetailsDiv.innerHTML = '';
 
-  handleSongTitleClick(songs);
-  handleEllipsisClick();
-  setUpSortingListeners(songs);
-}
+// //   const songDetailsHTML = renderSongDetails(songs);
+// //   songDetailsDiv.innerHTML = songDetailsHTML;
+
+// //   handleSongTitleClick(songs);
+// //   handleEllipsisClick();
+// //   setUpSortingListeners(songs);
+// // }
